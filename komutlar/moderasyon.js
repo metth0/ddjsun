84e8c3230fee700c466b2553dd1dfa7dfaae1d08
Glitch (sprinkle-bald-duke) <none>
2020-08-05 12:45:33 +0000

    🌱💐 Checkpoint ./komutlar/ayarmenu.js:15712673/20 ./komutlar/moderasyon.js:15712673/12 ./ayarlar.json:15712673/156

--- komutlar/moderasyon.js
+++ komutlar/moderasyon.js
@@ -5,12 +5,12 @@ let yardım = new Discord.RichEmbed()
 .setAuthor(`${client.user.username}`, client.user.avatarURL)
 .setColor('PURPLE')
 .addField('Diablo Moderasyon Menüsü',`
-**:white_small_square: = \`+modlog\` : Sunucunuzun Denetim Kaydını Tutar**
-**:white_small_square: = \`+reklamkick\` : Reklam Yapanları Kickler**
-**:white_small_square: = \`+ban\` : İstediğiniz Kişiyi Banlarsınız**
-**:white_small_square: = \`+kick\` : İstediğiniz Kişiyi Kicklersiniz**
-**:white_small_square: = \`+küfür-engel\` : Sunucunuzda Küfür Engel Açarr**
-**:white_small_square: = \`+sil 1-100\` : Belirtilen Kadar Mesaj Siler **`)
+**:white_small_square: = \`-modlog\` : Sunucunuzun Denetim Kaydını Tutar**
+**:white_small_square: = \`-reklamkick\` : Reklam Yapanları Kickler**
+**:white_small_square: = \`-ban\` : İstediğiniz Kişiyi Banlarsınız**
+**:white_small_square: = \`-kick\` : İstediğiniz Kişiyi Kicklersiniz**
+**:white_small_square: = \`-küfür-engel\` : Sunucunuzda Küfür Engel Açarr**
+**:white_small_square: = \`-sil 1-100\` : Belirtilen Kadar Mesaj Siler **`)
 .setFooter(`${message.author.tag} Tarafından İstendi.`, message.author.avatarURL)
 .setThumbnail(client.user.avatarURL)
  message.channel.send(yardım) 
